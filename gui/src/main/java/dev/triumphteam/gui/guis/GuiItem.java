9e6b2025657e869ba1d566fd5dfb0038a34be8b1
Simon Maribo <simonmaribo@outlook.dk>
2022-07-29 22:27:04 +0200

    Updated to 3.3: - updated to spigot 1.19

--- gui/src/main/java/dev/triumphteam/gui/guis/GuiItem.java
+++ gui/src/main/java/dev/triumphteam/gui/guis/GuiItem.java
@@ -25,7 +25,6 @@ package dev.triumphteam.gui.guis;
 
 import dev.triumphteam.gui.components.GuiAction;
 import dev.triumphteam.gui.components.util.ItemNbt;
-import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
@@ -57,7 +56,6 @@ public class GuiItem {
      * @param action    The {@link GuiAction} to run when clicking on the Item
      */
     public GuiItem(@NotNull final ItemStack itemStack, @Nullable final GuiAction<@NotNull InventoryClickEvent> action) {
-        Validate.notNull(itemStack, "The ItemStack for the GUI Item cannot be null!");
 
         this.action = action;
 
@@ -99,7 +97,6 @@ public class GuiItem {
      * @param itemStack The new {@link ItemStack}
      */
     public void setItemStack(@NotNull final ItemStack itemStack) {
-        Validate.notNull(itemStack, "The ItemStack for the GUI Item cannot be null!");
         this.itemStack = ItemNbt.setString(itemStack, "mf-gui", uuid.toString());
     }
 
